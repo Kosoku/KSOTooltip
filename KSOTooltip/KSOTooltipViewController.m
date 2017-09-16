649506e6d0ee1e8a5239eb52beb01855ec8e00b7
William Towe <willbur1984@gmail.com>
2017-09-16 18:59:42 -0400

    remove presentation controller

--- KSOTooltip/KSOTooltipViewController.m
+++ KSOTooltip/KSOTooltipViewController.m
@@ -14,7 +14,6 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "KSOTooltipViewController.h"
-#import "KSOTooltipPresentationController.h"
 #import "KSOTooltipAnimation.h"
 #import "KSOTooltipView.h"
 
@@ -79,9 +78,6 @@
 }
 - (id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed {
     return [[KSOTooltipAnimation alloc] init];
-}
-- (UIPresentationController *)presentationControllerForPresentedViewController:(UIViewController *)presented presentingViewController:(UIViewController *)presenting sourceViewController:(UIViewController *)source {
-    return [[KSOTooltipPresentationController alloc] initWithPresentedViewController:presented presentingViewController:presenting];
 }
 #pragma mark *** Public Methods ***
 #pragma mark Properties
