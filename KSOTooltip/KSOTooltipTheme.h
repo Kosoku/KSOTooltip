424ed93d9590c0c7e572196d766ba42535294c70
William Towe <willbur1984@gmail.com>
2021-06-12 10:26:44 -0400

    Update copyright year

--- KSOTooltip/KSOTooltipTheme.h
+++ KSOTooltip/KSOTooltipTheme.h
@@ -3,7 +3,7 @@
 //  KSOTooltip
 //
 //  Created by William Towe on 9/18/17.
-//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//  Copyright © 2021 Kosoku Interactive, LLC. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
